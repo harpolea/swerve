7e74aa9c2187810ab9ea2d8d3f749894e1d2ae98
Alice Harpole <alice.harpole@soton.ac.uk>
2017-02-24 10:24:02 +0000

    Getting docs to work on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    subprocess.call('cd ../doxygen; doxygen', shell=True)
+    subprocess.call('cd ..; doxygen', shell=True)
 
 
 # -- General configuration ------------------------------------------------
